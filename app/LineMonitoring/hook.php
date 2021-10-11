18864558532a570b7b977833d7f0a438db082de1
siavash sepahi <sepahisiavash@gmail.com>
2021-10-11 20:31:29 +0330

    fix units filed service

--- app/LineMonitoring/hook.php
+++ app/LineMonitoring/hook.php
@@ -71,48 +71,8 @@ class hook extends pluginController
     public function _fieldService_listOfTypes($vars2)
     {
         return [
-            ['type' => 'units', 'name' => 'واحد ها'],
             ['type' => 'phase', 'name' => 'فاز ها'],
         ];
-    }
-
-    public function _fieldService_showToFillOut_units($vars2)
-    {
-        $model = $this->model(['LineMonitoring', 'units']);
-        $searchFathers = $model->search('1', ' ?  ', null, '*', ['column' => 'Name', 'type' => 'asc']);
-        $options = '';
-        if (is_array($searchFathers))
-            foreach ($searchFathers as $search) {
-                $selected = '';
-                if (isset($this->mold->get('Mold')['post']['customField'][$this->mold->get('field')['fieldId']])) {
-                    if (in_array($search['unitId'], $this->mold->get('Mold')['post']['customField'][$this->mold->get('field')['fieldId']]))
-                        $selected = 'selected';
-                } elseif (isset($this->mold->get('field')['value'])) {
-                    $explodeSelectedValue = explode(' - ', $this->mold->get('field')['value']);
-                    if (in_array($search['unitId'], $explodeSelectedValue))
-                        $selected = 'selected';
-                }
-                $options .= '<option value ="' . $search['unitId'] . '" ' . $selected . '>' . $search['Name'] . '</option>';
-
-            }
-        $html = '<div class="' . $this->mold->get('fillOutFieldServiceFormCssClassAllDiv') . '">
-    <label class="' . $this->mold->get('fillOutFieldServiceFormCssClassLabelDiv') . '" for="field_' . $this->mold->get('field')['fieldId'] . '">' . $this->mold->get('field')['title'] . ' ' . (($this->mold->get('field')['status'] == 'required' and !$this->mold->get('shouldNotUserRequired')) ? '<span class="text-danger">*</span>' : '') . '</label>
-    <div class="' . $this->mold->get('fillOutFieldServiceFormCssClassInputDiv') . '">
-        <select  autocomplete="off" class="selectpicker" id="field_' . $this->mold->get('field')['fieldId'] . '"  name="customField[' . $this->mold->get('field')['fieldId'] . '][]" ' . (($this->mold->get('field')['status'] == 'required' and !$this->mold->get('shouldNotUserRequired')) ? 'required' : '') . ' data-size="7" data-style="btn btn-outline-info btn-round text-right" title="' . rlang(['please', 'selecting']) . '">
-        ' . $options . '
-        </select>
-        ' . (($this->mold->get('field')['description'] != '') ? '<div class="small text-gray">' . $this->mold->get('field')['description'] . '</div>' : '') . '
-    </div>
-</div>';
-
-        return $html;
-
-    }
-
-    public function _fieldService_showValue_units($fieldInformation = null)
-    {
-        $model = $this->model(['LineMonitoring', 'units'], $fieldInformation['value']);
-        return $model->getName();
     }
 
     public function _fieldService_showToFillOut_phase($vars2)

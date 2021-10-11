2ada6c48d20a8d51ceae1d9fc3ec5161114db23b
siavash sepahi <sepahisiavash@gmail.com>
2021-10-11 22:38:04 +0330

    create new log method from class

--- app/ElectricalSubstation/app_provider/api/electrical.php
+++ app/ElectricalSubstation/app_provider/api/electrical.php
@@ -12,9 +12,9 @@ use paymentCms\component\request;
 
 if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
 
-class elec_newLog extends innerController
+class electrical extends innerController
 {
-    public static function index(): array
+    public static function newLog(): array
     {
         $DataArray = json_decode($_POST['DataArray']);
         if (isset($DataArray)) {
--- app/LineMonitoring/app_provider/api/line_monitoring.php
+++ app/LineMonitoring/app_provider/api/line_monitoring.php
@@ -1,49 +1,67 @@
 <?php
-
-
 namespace App\LineMonitoring\app_provider\api;
 
-
 use App\api\controller\innerController;
+use App\LineMonitoring\model\sensor_active_log_archive;
 use App\shiftWork\app_provider\api\shift;
+use app\LineMonitoring\model\data;
 use app\LineMonitoring\model\sensors;
-use app\LineMonitoring\model\sensor_active_log_archive;
 use paymentCms\component\cache;
 use paymentCms\component\JDate;
 use paymentCms\component\model;
 use paymentCms\component\request;
 use paymentCms\component\validate;
 
-
 if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
 
-
-class chageStatus extends innerController {
-
-	/**
-	 * @return array
-	 *              [global-access]
-	 */
-	public static function index(){
-		$data = request::post( 'Sensor_id,time,active,Tile_Kind');
-		$rules =[
-			'Sensor_id'	=> ['required', 'شماره یکتای سنسور'],
-			'active'	=> ['required', 'وضعیت'],
-			'Tile_Kind'	=> ['required', 'نوع کاشی'],
-		];
-		$valid = validate::check($data, $rules);
-		if ($valid->isFail()) {
-			return self::jsonError($valid->errorsIn());
+class line_monitoring extends innerController {
+	public  static function newLog(){
+		$port = parent::setting('wsPort' , 'LineMonitoring' , true);
+		$ipsInString = parent::setting('WsIP' , 'LineMonitoring', true);
+		$ips = preg_split('/\r\n|[\r\n]/', $ipsInString) ;
+		require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'web_socket'.DIRECTORY_SEPARATOR.'clientSocket.php';
+        $DataArray = json_decode($_POST['DataArray']);
+		if ( isset($DataArray) ){
+			foreach ($DataArray  as $index => $_dataSTD ){
+                $_data = [
+                            "Sensor_id" => $_dataSTD->Sensor_id,
+                            "AbsTime" => $_dataSTD->AbsTime,
+                            "counter" => $_dataSTD->counter,
+                            "Tile_Kind" => $_dataSTD->Tile_Kind,
+                            "Motor_Speed" => $_dataSTD->Motor_Speed,
+                            "start_time" => $_dataSTD->start_time,
+                        ];
+				$result = self::insertLog($_data , $ips , $port);
+				if ( ! $result[0] ){
+					return self::jsonError(['indexOfProblem' => $index , 'error' => $result[1]]);
+				}
+			}
+		} else {
+			$result = self::insertLog($_POST , $ips , $port);
+			if ( ! $result[0] ){
+				return self::jsonError(['indexOfProblem' => 0 , 'error' => $result[1]]);
+			}
 		}
 
+
+		$data       = cache::get('isTileKindUpdate'   , null ,'LineMonitoring');
+		$dataSwitch = cache::get('isSwitchKindUpdate' , null ,'LineMonitoring');
+		if ( $data !== 'yes' or $dataSwitch !== 'yes' ) {
+			return self::jsonError(null,205);
+		}
+
+		return self::jsonError(null,204);
+	}
+	private static function insertLog($_data , $ips , $port){
+		$data = request::getFromArray( $_data,'Sensor_id,AbsTime,counter,Tile_Kind,Motor_Speed,start_time');
 		/* @var sensors $sensor */
-		$sensor = parent::model(['LineMonitoring','sensors'] , [$data['Sensor_id'] ] , 'id = ? ');
+		$sensor = parent::model(['LineMonitoring','sensors'] , [$data['Sensor_id'] ] , 'Sensor_id = ? ');
 		if ( $sensor->getSensorId() != $data['Sensor_id'] ) {
-			return self::jsonError('شماره یکتا سنسور یافت نشد!');
+			return [false,'شماره یکتا سنسور یافت نشد!'];
 		}
-        
-        $Time = $data['time'];
-        
+
+		$Time = $data['start_time'];
+
 		if ( $Time == null )
 			$Time = date('Y-m-d H:i:s');
         $strTime = strtotime($Time);
@@ -51,21 +69,109 @@ class chageStatus extends innerController {
 		if ( $shifts = cache::get('lastShiftGet' , null ,'LineMonitoring') ){
 			if ( ! ($strTime <= $shifts['endTimeStamp'] and $strTime >= $shifts['startTimeStamp'] ) ) {
 				$_SERVER['JsonOff'] = true;
-                $shifts =  shift::index($strTime)['result'];
+                $shifts = shift::index($strTime)['result'];
                 unset($_SERVER['JsonOff']);
 			}
 
 		} else {
 			$_SERVER['JsonOff'] = true;
-            $shifts =  shift::index($strTime)['result'];
+            $shifts = shift::index($strTime)['result'];
             unset($_SERVER['JsonOff']);
 		}
-        
+
         $shiftId = $shifts['shift_id'];
         $shiftWorker = $shifts['taskmaster_id'];
         $shift_time_group = $shifts['shift_time_group'];
 
-		/* @var sensor_active_log_archive $logArchive */
+		/* @var data $log */
+		$log = parent::model(['LineMonitoring','data_temp'] );
+
+		$log->setStartTime($Time);
+		$log->setAbsTime($data['AbsTime']);
+		$log->setSensorId($sensor->getSensorId());
+		$log->setShiftId($shiftId);
+		$log->setEmployersId($shiftWorker);
+		$log->setShiftGroupId($shift_time_group);
+		$log->setCounter($data['counter']);
+		$log->setTileKind($data['Tile_Kind']);
+		$log->setMotorSpeed($data['Motor_Speed']);
+		$log->setPhase($sensor->getPhase());
+		$log->setUnit($sensor->getUnitId());
+		$log->setTileDegree($sensor->getTileDegree());
+		$log->setJStartTime(JDate::jdate('Y/n/j',$strTime));
+		if ( ! $log->insertToDataBase() ) {
+			return [false,model::getLastQuery()];
+		}
+
+
+		for ( $i =  0 ; $i < count($ips) ; $i++ ) {
+			$ip = trim($ips[$i]);
+			$ip = str_replace(['https://' , 'http://' , '/','https:','http:'] , '' , $ip );
+			if ( $ip != '' and $port != '' ){
+				$ws = new clientSocket(array('host' => $ip,'port' => $port,'path' => ''));
+				$ws->send(json_encode([
+					'sensor_id' => $sensor->getSensorId() ,
+					'label' => $sensor->getLabel(),
+					'tile_id' => $log->getTileKind(),
+					'shift_id' => $log->getShiftId(),
+					'counter' => $log->getCounter(),
+					'absTime' => $log->getAbsTime(),
+					'motorSpeed' => $log->getMotorSpeed(),
+					'phase' => $sensor->getPhase(),
+					'unitId' => $sensor->getUnitId(),
+					'tileDegree' => $sensor->getTileDegree(),
+				]));$ws->send('disconnect');
+				$ws->close();
+			}
+		}
+
+
+		return [true];
+	}
+
+
+    public static function sensorActivity(){
+        $data = request::post( 'Sensor_id,time,active,Tile_Kind');
+        $rules =[
+            'Sensor_id'	=> ['required', 'شماره یکتای سنسور'],
+            'active'	=> ['required', 'وضعیت'],
+            'Tile_Kind'	=> ['required', 'نوع کاشی'],
+        ];
+        $valid = validate::check($data, $rules);
+        if ($valid->isFail()) {
+            return self::jsonError($valid->errorsIn());
+        }
+
+        /* @var sensors $sensor */
+        $sensor = parent::model(['LineMonitoring','sensors'] , [$data['Sensor_id'] ] , 'id = ? ');
+        if ( $sensor->getSensorId() != $data['Sensor_id'] ) {
+            return self::jsonError('شماره یکتا سنسور یافت نشد!');
+        }
+
+        $Time = $data['time'];
+
+        if ( $Time == null )
+            $Time = date('Y-m-d H:i:s');
+        $strTime = strtotime($Time);
+
+        if ( $shifts = cache::get('lastShiftGet' , null ,'LineMonitoring') ){
+            if ( ! ($strTime <= $shifts['endTimeStamp'] and $strTime >= $shifts['startTimeStamp'] ) ) {
+                $_SERVER['JsonOff'] = true;
+                $shifts =  shift::index($strTime)['result'];
+                unset($_SERVER['JsonOff']);
+            }
+
+        } else {
+            $_SERVER['JsonOff'] = true;
+            $shifts =  shift::index($strTime)['result'];
+            unset($_SERVER['JsonOff']);
+        }
+
+        $shiftId = $shifts['shift_id'];
+        $shiftWorker = $shifts['taskmaster_id'];
+        $shift_time_group = $shifts['shift_time_group'];
+
+        /* @var sensor_active_log_archive $logArchive */
         if ( $data['active'] == 1 ){
             $logArchive = parent::model(['LineMonitoring','sensor_active_log_archive'] , [$sensor->getSensorId() , ''] , ' Sensor_id = ? and ( End_Time is null or End_Time = ? ) ');
             if ( $logArchive->getSensor_id() == $sensor->getSensorId() ){
@@ -78,7 +184,7 @@ class chageStatus extends innerController {
                 if ( $logArchive->upDateDataBase() ){
                     $sensor->setActive(1);
                     $sensor->upDateDataBase();
-                    
+
                     return self::json(null);
                 }
                 return self::jsonError(model::getLastQuery());
@@ -88,7 +194,7 @@ class chageStatus extends innerController {
                 return self::jsonError('Cant Find Deactive log!');
             }
         } else {
-             $logArchive = parent::model(['LineMonitoring','sensor_active_log_archive'] , [$sensor->getSensorId() , ''] , ' Sensor_id = ? and ( End_Time is null or End_Time = ? ) ');
+            $logArchive = parent::model(['LineMonitoring','sensor_active_log_archive'] , [$sensor->getSensorId() , ''] , ' Sensor_id = ? and ( End_Time is null or End_Time = ? ) ');
             if ( $logArchive->getSensor_id() != $sensor->getSensorId() ){
                 $logArchive->setSensor_id( $sensor->getSensorId() );
                 $logArchive->setPhase( $sensor->getPhase() );
@@ -120,40 +226,40 @@ class chageStatus extends innerController {
                 $sensor->upDateDataBase();
                 return self::jsonError('duplicate request!');
             }
-        }        
-        
-	}
-    
-    public static function camSwitch(){
-		$data = request::post( 'id,time,active');
-		$Switch = parent::model(['LineMonitoring','CamSwitch'] , [$data['id'] ] , 'id = ? ');
-		if ( $Switch->getid() != $data['id'] ) {
-			return self::jsonError('شماره یکتا کلید یافت نشد!');
-		}
+        }
+
+    }
+
+    public static function camSwitchActivity(){
+        $data = request::post( 'id,time,active');
+        $Switch = parent::model(['LineMonitoring','CamSwitch'] , [$data['id'] ] , 'id = ? ');
+        if ( $Switch->getid() != $data['id'] ) {
+            return self::jsonError('شماره یکتا کلید یافت نشد!');
+        }
 
         $Time = $data['time'];
-        
-		if ( $Time == null )
-			$Time = date('Y-m-d H:i:s');
+
+        if ( $Time == null )
+            $Time = date('Y-m-d H:i:s');
         $strTime = strtotime($Time);
 
-		if ( $shifts = cache::get('lastShiftGet' , null ,'LineMonitoring') ){
-			if ( ! ($strTime <= $shifts['endTimeStamp'] and $strTime >= $shifts['startTimeStamp'] ) ) {
-				$_SERVER['JsonOff'] = true;
+        if ( $shifts = cache::get('lastShiftGet' , null ,'LineMonitoring') ){
+            if ( ! ($strTime <= $shifts['endTimeStamp'] and $strTime >= $shifts['startTimeStamp'] ) ) {
+                $_SERVER['JsonOff'] = true;
                 $shifts = shift::index($strTime)['result'];
                 unset($_SERVER['JsonOff']);
-			}
+            }
 
-		} else {
-			$_SERVER['JsonOff'] = true;
+        } else {
+            $_SERVER['JsonOff'] = true;
             $shifts =  shift::index($strTime)['result'];
             unset($_SERVER['JsonOff']);
-		}
-        
+        }
+
         $shiftId = $shifts['shift_id'];
         $shiftWorker = $shifts['taskmaster_id'];
         $shift_time_group = $shifts['shift_time_group'];
-        
+
         if ( $data['active'] == 1 ){
             $logArchive = parent::model(['LineMonitoring','Switch_active_log_archive'] , [$Switch->getId() , ''] , ' id = ? and ( End_Time is null or End_Time = ? ) ');
             if ($logArchive->getId() == $Switch->getId() ){
@@ -165,11 +271,11 @@ class chageStatus extends innerController {
                 if ( $logArchive->upDateDataBase() ){
                     $Switch->setActive(1);
                     $Switch->upDateDataBase();
-                    
+
                     $log  = parent::model(['LineMonitoring','Switch_active_log']  );
                     $log->setFromArray($logArchive->returnAsArray());
                     $log->updateOneRow();
-                    
+
                     return self::json(null);
                 }
                 return self::jsonError(model::getLastQuery());
@@ -179,7 +285,7 @@ class chageStatus extends innerController {
                 return self::jsonError('Cant Find Deactive log!');
             }
         } else {
-            
+
             $logArchive = parent::model(['LineMonitoring','Switch_active_log_archive'] , [$Switch->getId() , ''] , ' id = ? and ( End_Time is null or End_Time = ? ) ');
             if ( $logArchive->getId() != $Switch->getId() ){
                 $logArchive->setId( $Switch->getId() );
@@ -196,15 +302,15 @@ class chageStatus extends innerController {
                 $logArchive->setEnd_Shift_group_id( null );
                 $logArchive->setEnd_employers_id( null );
                 $logArchive->setReason( null );
-                $logArchive->setDescription( null ); 
+                $logArchive->setDescription( null );
                 if ( $logArchive->insertToDataBase() ){
                     $Switch->setActive(0);
                     $Switch->upDateDataBase();
-                    
+
                     $log  = parent::model(['LineMonitoring','Switch_active_log']  );
                     $log->setFromArray($logArchive->returnAsArray());
                     $log->insertToDataBase();
-                    
+
                     return self::json(null);
                 }
                 return self::jsonError(model::getLastQuery());
@@ -213,9 +319,9 @@ class chageStatus extends innerController {
                 $Switch->upDateDataBase();
                 return self::jsonError('duplicate request!');
             }
-        }        
-        
-	}
+        }
+
+    }
 }
 
 

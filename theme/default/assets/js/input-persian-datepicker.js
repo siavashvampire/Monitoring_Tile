ca9cd8fce380d9f95f3f03728fa8b675e955b72b
siavash sepahi <sepahisiavash@gmail.com>
2021-10-12 15:38:32 +0330

    update persian date picker

--- theme/default/assets/js/input-persian-datepicker.js
+++ theme/default/assets/js/input-persian-datepicker.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var to_persianDatepicker, from_persianDatepicker;
-    var to1, from1;
+    var to1, from1,to2, from2,to3, from3;
     var persianNumber1 = ["۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"] ;
     to1 = $(".persianDatepicker-to").persianDatepicker({
         altField: '.persianDatepicker-to-alt',
@@ -8,7 +8,7 @@ $(document).ready(function() {
         format: 'YYYY/MM/DD ساعت HH:mm',
         title:'ss',
         initialValue: false,
-		maxDate: new persianDate().valueOf(),
+        maxDate: new persianDate().valueOf(),
         autoClose: true,
         timePicker: {
             enabled: true,
@@ -37,8 +37,8 @@ $(document).ready(function() {
         altField: '.persianDatepicker-from-alt',
         persianDigit: false ,
         initialValue: false,
-		minDate : null,
-		maxDate: new persianDate().valueOf(),
+        minDate : null,
+        maxDate: new persianDate().valueOf(),
         format: 'YYYY/MM/DD ساعت HH:mm',
         timePicker: {
             enabled: true,
@@ -60,6 +60,129 @@ $(document).ready(function() {
                 to1.options = {minDate: unix};
                 if (to1.touched) {
                     to1.setDate(cachedValue);
+                }
+            }
+        }
+    });
+
+    to2 = $(".persianJustDatepicker-to").persianDatepicker({
+        altField: '.persianJustDatepicker-to-alt',
+        persianDigit: false ,
+        format: 'YYYY/MM/DD',
+        title:'ss',
+        initialValue: false,
+        autoClose: true,
+        timePicker: {
+            enabled: true,
+            meridiem: {
+                enabled: false
+            },
+            second : {
+                enabled: false
+            },
+            minute : {
+                enabled: true
+            }
+        },
+        onSelect: function (unix) {
+            to2.touched = true;
+            if (from2 && from2.options && from2.options.maxDate != unix  ) {
+                var cachedValue = from2.getState().selected.unixDate;
+                from2.options = {maxDate: unix  };
+                if (from2.touched) {
+                    from2.setDate(cachedValue);
+                }
+            }
+        }
+    });
+    from2 = $(".persianJustDatepicker-from").persianDatepicker({
+        altField: '.persianDatepicker-from-alt',
+        persianDigit: false ,
+        initialValue: false,
+        minDate : null,
+        format: 'YYYY/MM/DD',
+        timePicker: {
+            enabled: true,
+            meridiem: {
+                enabled: false
+            },
+            second : {
+                enabled: false
+            },
+            minute : {
+                enabled: true
+            }
+        },
+        autoClose: true,
+        onSelect: function (unix) {
+            from2.touched = true;
+            if (to2 && to2.options && to2.options.minDate != unix ) {
+                var cachedValue = to2.getState().selected.unixDate;
+                to2.options = {minDate: unix};
+                if (to2.touched) {
+                    to2.setDate(cachedValue);
+                }
+            }
+        }
+    });
+
+
+    to3 = $(".persianJustDatepicker-to2").persianDatepicker({
+        altField: '.persianJustDatepicker-to-alt2',
+        persianDigit: false ,
+        format: 'YYYY/MM/DD',
+        title:'ss',
+        initialValue: false,
+        autoClose: true,
+        timePicker: {
+            enabled: true,
+            meridiem: {
+                enabled: false
+            },
+            second : {
+                enabled: false
+            },
+            minute : {
+                enabled: true
+            }
+        },
+        onSelect: function (unix) {
+            to3.touched = true;
+            if (from3 && from3.options && from3.options.maxDate != unix  ) {
+                var cachedValue = from3.getState().selected.unixDate;
+                from3.options = {maxDate: unix  };
+                if (from3.touched) {
+                    from3.setDate(cachedValue);
+                }
+            }
+        }
+    });
+    from3 = $(".persianJustDatepicker-from2").persianDatepicker({
+        altField: '.persianDatepicker-from-alt2',
+        persianDigit: false ,
+        initialValue: false,
+        minDate : null,
+        format: 'YYYY/MM/DD',
+        timePicker: {
+            enabled: true,
+            meridiem: {
+                enabled: false
+            },
+            second : {
+                enabled: false
+            },
+            minute : {
+                enabled: true
+            }
+        },
+        autoClose: true,
+        onSelect: function (unix) {
+            from3.touched = true;
+            if (to3 && to3.options && to3.options.minDate != unix ) {
+                var cachedValue = to3.getState().selected.unixDate;
+                to3.options = {minDate: unix};
+                if (to3.touched) {
+                    to3.setDate(cachedValue);
                 }
             }
         }

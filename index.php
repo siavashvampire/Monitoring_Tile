df532b36f81122a17c5bf8c562becbcd69e6aef0
ali_ytn <erfan.yaghtin.79@gmail.com>
2021-10-17 18:02:07 +0330

    gitignore update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 $GLOBALS['timeStart'] = ( 1 ) ? microtime() : '';
 define( 'paymentCMS' , true);
 define('payment_path' , __DIR__.DIRECTORY_SEPARATOR);
-define('debugger' , 1);
+define('debugger' , 0);
 require_once __DIR__ . '/core/boot/init.php';
 app::init();

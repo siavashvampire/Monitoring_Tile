3f07964f6d0d5976c48a29d1f3f203e2edcb1c0f
siavash sepahi <sepahisiavash@gmail.com>
2021-10-17 15:53:39 +0330

    fix cronjob

--- app/shiftWork/app_provider/api/shift.php
+++ app/shiftWork/app_provider/api/shift.php
@@ -2,23 +2,27 @@
 namespace App\shiftWork\app_provider\api;
 
 use App\api\controller\innerController;
+use App\shiftWork\model\shift_time;
 use paymentCms\component\JDate;
-use paymentCms\component\model;
 
 
-if ( !defined( 'paymentCMS' ) ) die( '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>' );
+if ( !defined( 'paymentCMS' ) ) die( '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>' );
 
 class shift extends innerController {
-	public  static function index ($date = null ){
+	public  static function index ($date = null ): array
+    {
 		if ( $date == null )  $date = time() ;
 		$day = JDate::jdate('l' , $date);
 		$dayNum = JDate::jdate('w' , $date);
 		$timeSTR = date("H:i:30" , $date);
+        $startDateTime = null;
+        $endDateTime = null;
 
-		$shift = parent::model(['shiftWork', 'shift_time']);
-		model::join('shift_time shift_time2', 'shift_time.shift_id = shift_time2.shift_id and shift_time.shift_time_group = shift_time2.shift_time_group ' , 'right');
-		model::join('shift_work shift_work', 'shift_time.shift_id = shift_work.shift_id');
-		$shifts = $shift->search([$day, $timeSTR , -1], ' (shift_time.onDay = ? and ? BETWEEN  shift_time.startTime and shift_time.endTime) AND shift_time.shift_id <> ?', 'shift_time shift_time', 'shift_work.taskmaster_id,shift_time.shift_id,shift_time.shift_time_group , shift_time2.startTime , shift_time2.endTime , shift_time2.onDay as day , CASE WHEN shift_time2.onDay = "شنبه" then 0 WHEN shift_time2.onDay = "یک شنبه" then 1 WHEN shift_time2.onDay = "دو شنبه" then 2 WHEN shift_time2.onDay = "سه شنبه" then 3 WHEN shift_time2.onDay = "چهار شنبه" then 4 WHEN shift_time2.onDay = "پنج شنبه" then 5 ELSE 6 END AS daynum');
+        /** @var shift_time $model */
+        $model = parent::model(['shiftWork', 'shift_time']);
+        $model->join('shift_time shift_time2', 'shift_time.shift_id = shift_time2.shift_id and shift_time.shift_time_group = shift_time2.shift_time_group ' , 'right');
+        $model->join('shift_work shift_work', 'shift_time.shift_id = shift_work.shift_id');
+		$shifts = $model->search([$day, $timeSTR , -1], ' (shift_time.onDay = ? and ? BETWEEN  shift_time.startTime and shift_time.endTime) AND shift_time.shift_id <> ?', 'shift_time shift_time', 'shift_work.taskmaster_id,shift_time.shift_id,shift_time.shift_time_group , shift_time2.startTime , shift_time2.endTime , shift_time2.onDay as day , CASE WHEN shift_time2.onDay = "شنبه" then 0 WHEN shift_time2.onDay = "یک شنبه" then 1 WHEN shift_time2.onDay = "دو شنبه" then 2 WHEN shift_time2.onDay = "سه شنبه" then 3 WHEN shift_time2.onDay = "چهار شنبه" then 4 WHEN shift_time2.onDay = "پنج شنبه" then 5 ELSE 6 END AS daynum');
 		if ( $shifts !== true and count($shifts) > 0 ){
 			usort($shifts, function($a, $b) {
 				if ( $a['daynum'] - $b['daynum']  == 6 ){

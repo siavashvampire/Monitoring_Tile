d1502d85ad041c6c4bfcaebd1165f5e4e97a6f68
Hafez <sepahisiavash@gmail.com>
2021-10-20 19:14:15 +0330

    apply to main

--- change_new.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `per_customfieldvalue_7_user_register` SET `f_22`=10 WHERE `f_22` = 1;
-UPDATE `per_customfieldvalue_7_user_register` SET `f_22`=1 WHERE `f_22` = 3;
-UPDATE `per_customfieldvalue_7_user_register` SET `f_22`=2 WHERE `f_22` = 7;
-UPDATE `per_customfieldvalue_7_user_register` SET `f_22`=3 WHERE `f_22` = 'سایر';
-UPDATE `per_customfieldvalue_7_user_register` SET `f_22`=-4 WHERE `f_22` = 'همه';

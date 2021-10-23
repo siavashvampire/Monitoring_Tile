c5170b97ff5a810d24dbac9edf166fa61f795120
siavash sepahi <sepahisiavash@gmail.com>
2021-10-23 11:33:27 +0330

    update RS database

---
+++ change_new.sql
@@ -0,0 +1,4 @@
+UPDATE `per_requestservice` SET `System_Status`=1 WHERE `System_Status`=0;
+UPDATE `per_requestservice` SET `WorkTitle`=',1,' WHERE `WorkTitle`=',,';
+
+UPDATE `per_requestservice` SET `BugInfluence`=',1,' WHERE `BugInfluence`=',,';

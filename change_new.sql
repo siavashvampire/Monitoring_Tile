56dd35f33e97d4c8c0e3081ce963389f1edb8268
siavash sepahi <sepahisiavash@gmail.com>
2021-10-23 11:35:31 +0330

    system updated

--- change_new.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `per_requestservice` SET `System_Status`=1 WHERE `System_Status`=0;
-UPDATE `per_requestservice` SET `WorkTitle`=',1,' WHERE `WorkTitle`=',,';
-
-UPDATE `per_requestservice` SET `BugInfluence`=',1,' WHERE `BugInfluence`=',,';

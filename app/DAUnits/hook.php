fb3e07c4b22cd1c8bad9791e7324a94049e47240
siavash sepahi <sepahisiavash@gmail.com>
2021-10-27 15:30:05 +0330

    add update to DAunits and contacts

--- app/DAUnits/hook.php
+++ app/DAUnits/hook.php
@@ -2,6 +2,7 @@
 namespace App\DAUnits ;
 
 use app;
+use paymentCms\component\cache;
 use pluginController;
 
 if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
@@ -11,4 +12,18 @@ class hook extends pluginController {
         $this->menu->after('users' ,'DAUnits', rlang('DAUnits') , app::getBaseAppLink('DAUnits','admin') , 'fa fa-history' ,'',null,'admin/DAUnits/index/DAUnits');
         $this->menu->after('DAUnits' ,'DAUnitsList', rlang('DAUnitsList') , app::getBaseAppLink('DAUnits/List','admin') , 'fa fa-history' ,'',null,'admin/DAUnits/index/DAUnits');
 	}
+    public function _should_update()
+    {
+        $should_update = array();
+
+//        $data = cache::get('isSubstation', null, 'ElectricalSubstation');
+//
+//        if ($data !== 'yes')
+//            $should_update[] = "TileKindUpdate";
+
+        if (count($should_update) == 0)
+            return null;
+
+        return $should_update;
+    }
 }
